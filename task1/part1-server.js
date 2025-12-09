2d063eacdfc79fd17257a36dbc0bae54d777fc88
Aneni Kidanu Manale <aneni.kidanu@aastustudent.edu.et>
2025-12-10 00:14:19 +0300

    Update part1-server.js

--- task1/part1-server.js
+++ task1/part1-server.js
@@ -2,42 +2,29 @@ const http = require('http');
 
 const PORT = 3000;
 
-// In-memory storage for POST data
 let receivedData = null;
 
 const server = http.createServer((req, res) => {
-    // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
-    // Handle preflight requests
     if (req.method === 'OPTIONS') {
         res.writeHead(200);
         res.end();
         return;
     }
-    
-    // Set default content type
     res.setHeader('Content-Type', 'application/json');
-    
-    // Parse URL
     const url = req.url;
     const method = req.method;
-    
-    // Route handling
     if (url === '/' && method === 'GET') {
-        // GET /
         res.writeHead(200);
         res.end(JSON.stringify({ message: "Welcome to the Home Page" }));
         
     } else if (url === '/info' && method === 'GET') {
-        // GET /info
         res.writeHead(200);
         res.end(JSON.stringify({ message: "This is the information page" }));
         
     } else if (url === '/submit' && method === 'POST') {
-        // POST /submit
         let body = '';
         
         req.on('data', chunk => {
@@ -57,7 +44,6 @@ const server = http.createServer((req, res) => {
         });
         
     } else if (url === '/submit' && method === 'GET') {
-        // GET /submit (to view last submitted data)
         res.writeHead(200);
         res.end(JSON.stringify({ 
             message: "Last submitted data (use POST to submit new data)",
@@ -65,7 +51,6 @@ const server = http.createServer((req, res) => {
         }));
         
     } else {
-        // 404 Not Found
         res.writeHead(404);
         res.end(JSON.stringify({ error: "Route not found" }));
     }
