7d8c20faecacbb54d5205bd7b93219ed24845c93
aneni <aneni.kidanu@aastustudent.edu.et>
2025-12-23 22:29:44 +0300

    Initial commit: add bookstore-api

---
+++ routes/bookRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const bookController = require("../controllers/bookController");
+const validateBook = require("../middleware/validateBook");
+
+// Routes
+router.get("/", bookController.getAllBooks);
+router.get("/:id", bookController.getBookById);
+router.post("/", validateBook, bookController.createBook);
+
+module.exports = router;
